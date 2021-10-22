7d797187e8c173cf947062e9492d928bf0f212c1
TrnThSn <saophaisoan23112002@gmail.com>
2021-10-22 14:36:10 +0700

    javafx

---
+++ src/sample/Controller.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+
+}
